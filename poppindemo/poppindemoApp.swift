248a1fd4a4e4722a13cc9b140d29a6e63c479825
rcuevas01 <rcuevastech@gmail.com>
2024-06-19 21:12:51 -0700

    Initial Commit

---
+++ poppindemo/poppindemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  poppindemoApp.swift
+//  poppindemo
+//
+//  Created by Ruben Cuevas on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct poppindemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
